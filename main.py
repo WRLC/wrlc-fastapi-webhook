767e3ecfd9e048f97b226a19c831db3cdc396754
Tom Boone <tbc@tomboone.com>
2025-05-22 12:01:29 -0400

    remove unused import

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 FastAPI is a modern, fast (high-performance), web framework for building APIs with Python 3.6+ based on standard Python
 type hints.
 """
-import json
 import logging
 import os
 import secrets
